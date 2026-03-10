f98aaccfee0b1ae84abbc79fbbdffe84f89ec3a9
Brent Tunnicliff <brent@tunnicliff.dev>
2026-03-10 23:42:34 +1100

    Fixed compile issue in tests.

--- Tests/UUIDVersionsTests/UUIDVersionTests.swift
+++ Tests/UUIDVersionsTests/UUIDVersionTests.swift
@@ -10,18 +10,18 @@ import Testing
 #endif
 
 struct UUIDVersionTests {
-    typealias UUIDVersion = UUIDVersions.UUIDVersion<MockUUIDGenerator>
+    typealias UUIDVersion = UUIDVersions.UUIDVersion
     private let mockUUIDGenerator = MockUUIDGenerator()
 
     @Test(arguments: UUIDVersion.Value.allCases)
     func idMapsToValue(_ argument: UUIDVersion.Value) {
-        let version = UUIDVersion(value: argument, generator: mockUUIDGenerator)
+        let version = UUIDVersion(argument, generator: mockUUIDGenerator)
         #expect(version.id == argument.rawValue)
     }
 }
 
 extension UUIDVersionTests {
-    struct MockUUIDGenerator: UUIDGenerator {
+    final class MockUUIDGenerator: UUIDGenerator {
         func new() -> UUID {
             preconditionFailure("Not implemented")
         }
